dbf156d1e5d272e91b0e21d08bad5d51ca40c7a9
HwanHo Lee <ghksgh45@gmail.com>
2021-01-06 07:09:10 +0900

    [FullStack] Youtube Clone Coding #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hi');
+import express from "express";
+const app = express()
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://locahost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from home');
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res , next) => {
+    console.log("I'm between");
+    next();
+};
+
+app.use(betweenHome); //middleware all route
+
+app.get("/",handleHome);
+
+app.get('/profile',handleProfile)
+
+app.listen(4000, handleListening);
