2680864e07af7c91968d37cd709a9f0a67f95935
Lee Hwan Ho <ghksgh45@gmail.com>
2021-01-12 22:30:59 +0900

    #2.18 Search Controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan"; // for log middleware
 import helmet from "helmet"; // for security middleware
 import cookieParser from "cookie-parser"; //for cookie parsing middleware
 import bodyParser from "body-parser"; // for html body & json Object Parsing middleware
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -11,11 +12,14 @@ import routes from "./routes";
 const app = express()
 
 app.set("view engine", "pug");
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use('/scripts', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
+
+app.use (localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users,userRouter); 
